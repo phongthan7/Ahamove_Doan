f1d84421fe6f71e7c67db82ba42f4e58674b1914
Hải Phong <nguyenquochaiphong2701@gmail.com>
2023-07-10 13:17:11 +0700

    Alo PhongDay2

--- Ahamove/src/app/app.module.ts
+++ Ahamove/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SetTicketComponent } from './set-ticket/set-ticket.component';
+import { Test2Component } from './test2/test2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SetTicketComponent
+    SetTicketComponent,
+    Test2Component
   ],
   imports: [
     BrowserModule,
